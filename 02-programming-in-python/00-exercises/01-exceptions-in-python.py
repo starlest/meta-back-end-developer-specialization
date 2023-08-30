1f437ba0918ad8146e1a2af661af751c3b339f6e
Edwin <starlest@gmail.com>
2023-08-30 20:46:13 +0700

    add exceptions exercise

---
+++ 02-programming-in-python/00-exercises/01-exceptions-in-python.py
@@ -0,0 +1,28 @@
+# IndexError
+items = [1,2,3,4,5]
+try:
+    item = items[6]
+    print(item)
+except IndexError:
+    print("Item does not exist in the list")
+
+# ZeroDivisionError
+def divide_by(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return 0
+    except Exception as e:
+        print(e, 'Something went wrong!')
+
+ans = divide_by(40, 0)
+print(ans)
+
+
+
+# FileNotFoundError
+try:
+    with open('file_does_not_exist.txt', 'r') as file:
+        print(file.read())
+except:
+    print("Unable to locate file")  
